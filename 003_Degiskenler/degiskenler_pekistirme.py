273e8c7249a42184b9d76eaaf4bb10894eecdfb1
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-17 23:29:55 +0300

    Create degiskenler_pekistirme.py

---
+++ 003_Degiskenler/degiskenler_pekistirme.py
@@ -0,0 +1,22 @@
+"""
+Aşağıdaki müşteri bilgilerinin ve satın aldığı ürün bilgilerini değişkenler içerisinde saklayınız.
+Toplam ödenen ücret nedir?
+Ödenen miktarın kdv oranı nedir? (%18)
+
+Müslüm H. Erol
+05554443322
+mslmhanerol@gmail.com
+Gaziantep
+
+Satın alınan ürünler
+
+Ürün adı: Kablosus Mouse
+Fiyatı: 550 TL
+
+Ürün adı: Kablosus Klavye
+Fiyatı: 650 TL
+
+Ürün adı: Dizüstü Bilgisayar
+Fiyatı: 55.000
+
+"""
