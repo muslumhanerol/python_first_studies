6c1b264d3d10b63e0f6bcd1340eec9c82362fa87
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-18 23:18:53 +0300

    type_re

--- 04-veri-tipleri/veri_tipi_donusumleri.py
+++ 04-veri-tipleri/veri_tipi_donusumleri.py
@@ -1,25 +1,39 @@
-sayi1= 10
-sayi2= 20
-toplam= sayi1 + sayi2
-print(toplam) #sonuç=30
+# sayi1= 10
+# sayi2= 20
+# toplam= sayi1 + sayi2
+# print(toplam) #sonuç=30
 
 
-sayi3= "10"
-sayi4= "20"
-toplam2= sayi3 + sayi4
-print(toplam2) #sonuç=1020
+# sayi3= "10"
+# sayi4= "20"
+# toplam2= sayi3 + sayi4
+# print(toplam2) #sonuç=1020
 
 
-#İnput ile veri girişli
-sayi5= input("sayı 5: ")
-sayi6= input("sayı 6: ")
-toplam = sayi5+sayi6
-print(toplam) #sonuç:girilen sayıları yanyana yazar.
+# #İnput ile veri girişli
+# sayi5= input("sayı 5: ")
+# sayi6= input("sayı 6: ")
+# toplam = sayi5+sayi6
+# print(toplam) #sonuç:girilen sayıları yanyana yazar.
 
 
-#İnput ile veri girişli ve tür dönüşümü
-sayi5= int(input("sayı 5: "))
-sayi6= int(input("sayı 6: "))
-toplam = sayi5+sayi6
-print(toplam) #sonuc: girilen sayıları matematiksel toplar.
+# #İnput ile veri girişli ve tür dönüşümü
+# sayi5= int(input("sayı 5: "))
+# sayi6= int(input("sayı 6: "))
+# toplam = sayi5+sayi6
+# print(toplam) #sonuc: girilen sayıları matematiksel toplar.
+
+
+
+# x = int("1")
+# x = int("10.5") #hata
+# x = int(10.5) #ondalıklı sayıyı atar tam sayıya çevirir.
+# x = float("10.4") #ondalıklı olarak yazar.
+# x= str(3.4)
+# x= str(True)
+# x= str(False)
+
+
+print(type(x))
+print(x)
 
