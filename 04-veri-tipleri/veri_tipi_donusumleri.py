410c6617dda6b5f4c6d5a820c94a3f5689447eb0
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-18 23:08:46 +0300

    int

---
+++ 04-veri-tipleri/veri_tipi_donusumleri.py
@@ -0,0 +1,25 @@
+sayi1= 10
+sayi2= 20
+toplam= sayi1 + sayi2
+print(toplam) #sonuç=30
+
+
+sayi3= "10"
+sayi4= "20"
+toplam2= sayi3 + sayi4
+print(toplam2) #sonuç=1020
+
+
+#İnput ile veri girişli
+sayi5= input("sayı 5: ")
+sayi6= input("sayı 6: ")
+toplam = sayi5+sayi6
+print(toplam) #sonuç:girilen sayıları yanyana yazar.
+
+
+#İnput ile veri girişli ve tür dönüşümü
+sayi5= int(input("sayı 5: "))
+sayi6= int(input("sayı 6: "))
+toplam = sayi5+sayi6
+print(toplam) #sonuc: girilen sayıları matematiksel toplar.
+
--- 04-veri-tipleri/veri_tipleri.py
+++ 04-veri-tipleri/veri_tipleri.py
@@ -6,3 +6,22 @@
 
 # Boolen
 # bool= koşullu durumları nitelemek
+
+
+isim= "Ali"
+yas= 7
+kilo= 15.5
+ogranciMi= True
+
+#Verilerin türlerini sorgulama.
+print(type(isim))  #str
+print(type(yas))   #int
+print(type(kilo))  #float
+print(type(ogranciMi)) #bool
+
+
+#print("ad: " + isim + " yas: " + yas + " kilo: " + kilo + " öğrenci mi: " + ogranciMi)
+# Yukarıdaki kod çalıştırıldığında tip hatası verir, bu hatayı önlemek için veri dönüşümü yapmalıyız.
+
+print("ad: " + isim + " yas: " + str(yas) + " kilo: " + str(kilo) + " öğrenci mi: " + str(ogranciMi))
+
