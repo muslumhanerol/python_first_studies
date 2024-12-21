8195557a9773d72bd16fd4278be2700e64173414
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-21 22:39:35 +0300

    Update string_slicing.py

--- 05-karakter-dizileri/string_slicing.py
+++ 05-karakter-dizileri/string_slicing.py
@@ -17,5 +17,18 @@ print(kurs[11:22]) #Son karakteri de almak istiyorsak bir fazlasını yaz. Çık
 print(kurs[11:len(kurs)]) # 11. indexten başla kursun sonuna kadar ne varsa al. Yukarıdakinin farklı bir yazımı. Çıktı= Programlama
 
 print(kurs[5:]) #Başlanğıç indexi verilip sonu verilmezse yine sonuna kadar gider. Çıktı= n ile Programlama
+print(kurs[11:]) #Çıktı= Programlama
+print(kurs[-11:-1]) # Son karakter alınmaz. Çıktı= Programlam
+
+
+#Seçerek yazdırma. Adım sayısı.
+print(kurs[0:20:1]) # 0 dan 20 ye kadar 1 er 1 er al. Çıktı= Python ile Programla
+print(kurs[0:20:2]) # 0 dan 20 ye kadar 2 şer 2 şer al. Çıktı= Pto l rgal
+
+
+print(kurs[::]) # Strigni olduğu gibi alır.
+print(kurs[::2]) # Strigni 2 şer 2 şer alır.
+print(kurs[::-1]) # Stringi ters çevirir. Çıktı= amalmargorP eli nohtyP
+
 
 
