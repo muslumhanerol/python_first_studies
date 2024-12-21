5aec48564cfaaef4d5d98f9ee800e03c0f3f1036
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-21 23:13:02 +0300

    Create string_formatlama.py

---
+++ 05-karakter-dizileri/string_formatlama.py
@@ -0,0 +1,15 @@
+# string concat //string birleştirme
+ad="Müslüm H."
+soyAd="Erol"
+yas=30
+
+mesaj="My name is " + ad + " " + soyAd + "." + "I am " + yas + " years old."
+
+
+
+# string format
+
+# f-string
+
+print(mesaj)
+ 
