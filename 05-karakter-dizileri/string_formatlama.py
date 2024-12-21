5585f53f5bf886f13e4ebb925c2b6f895d3b4e9c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-22 00:23:57 +0300

    Update string_formatlama.py

--- 05-karakter-dizileri/string_formatlama.py
+++ 05-karakter-dizileri/string_formatlama.py
@@ -2,14 +2,25 @@
 ad="Müslüm H."
 soyAd="Erol"
 yas=30
+#mesaj="My name is " + ad + " " + soyAd + "." + "I am " + str(yas) + " years old."
 
-mesaj="My name is " + ad + " " + soyAd + "." + "I am " + yas + " years old."
-
-
+#--------
 
 # string format
+#mesaj="My name {} {}. I am {} years old.".format(ad, soyAd, yas) # {} içine sırasıyla format içindekiler yerleşir. + operatörünü kullanmadan aynı sonucu aldık. Çıktı= My name Müslüm H. Erol. I am 30 years old.
+
+#mesaj="My name {1} {0}. I am {2} years old.".format(ad, soyAd, yas) # index numaralarını atadık. Çıktı= My name Erol Müslüm H.. I am 30 years old.
+
+
+#mesaj="My name {a} {s}. I am {y} years old.".format(a=ad, s=soyAd, y=yas) # Takma isim kullanma. Çıktı= My name Müslüm H. Erol. I am 30 years old.
+
+#--------
+
 
 # f-string
+
+mesaj="My name {a} {s}. I am {y} years old."
+
 
 print(mesaj)
  
