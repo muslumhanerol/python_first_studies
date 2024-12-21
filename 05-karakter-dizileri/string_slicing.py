dbe6625a6f22910a1840351b7ded067321207021
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-21 22:22:26 +0300

    slicing

---
+++ 05-karakter-dizileri/string_slicing.py
@@ -0,0 +1,15 @@
+kurs= "Python ile Programlama"
+
+#print(kurs[0]) # kurs değişkeninin ilk index karakteri gelir, yani P.
+
+#print(kurs[-1]) # kurs değişkeninin SON index karakteri gelir, yani a. -2 olsaydı m.
+
+
+adet = len(kurs) #Karakter sayısı öğrenme, boşluklar dahil edilir.
+print(adet)
+print(kurs[adet - 1]) #len fonksiyonu ile karakter sayısını bulur 1 eksiğini alır ve yine istediğimiz karaktere erişebiliriz. Burada a karakterine eriştik.
+
+
+print(kurs[0:5]) #Belirli bir index alanını alır. Burada 0 dan başlar 0 dahil 5 e kadar alır 5 hariç. Pytho çıktısı elde edilir.
+
+
--- 05-karakter-dizileri/string_veri_tipi.py
+++ 05-karakter-dizileri/string_veri_tipi.py
@@ -1,1 +1,11 @@
-print("hello")
+kursAdi="Python ile Programlama"
+kursAciklama="Güzel bir kurs"
+kursSuresi="20 Saat"
+
+mesaj = "Kurs Adı: "+kursAdi + " " + "Kurs Açıklaması: "+ kursAciklama + " " + "Kurs Süresi " + kursSuresi
+
+#print(mesaj) #Tüm mesajı yazdırır.
+
+#print(mesaj[-1]) #Mesajın son karakterini yazdırır.
+
+print(mesaj[0]) #Mesajın ilk karakterini yazdırır.
