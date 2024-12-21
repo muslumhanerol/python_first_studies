194a21d7419b813e85e559e5a77caa621f7dd467
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-21 22:30:52 +0300

    Update string_slicing.py

--- 05-karakter-dizileri/string_slicing.py
+++ 05-karakter-dizileri/string_slicing.py
@@ -5,11 +5,17 @@ kurs= "Python ile Programlama"
 #print(kurs[-1]) # kurs değişkeninin SON index karakteri gelir, yani a. -2 olsaydı m.
 
 
-adet = len(kurs) #Karakter sayısı öğrenme, boşluklar dahil edilir.
+adet = len(kurs) #Karakter sayısı öğrenme, boşluklar dahil edilir .
 print(adet)
 print(kurs[adet - 1]) #len fonksiyonu ile karakter sayısını bulur 1 eksiğini alır ve yine istediğimiz karaktere erişebiliriz. Burada a karakterine eriştik.
 
 
 print(kurs[0:5]) #Belirli bir index alanını alır. Burada 0 dan başlar 0 dahil 5 e kadar alır 5 hariç. Pytho çıktısı elde edilir.
+print(kurs[:5]) # Yukarıdakinin bir başka yazım şekli, aynı çıktıyı verir Pytho.
+
+print(kurs[11:22]) #Son karakteri de almak istiyorsak bir fazlasını yaz. Çıktı= Programlama
+print(kurs[11:len(kurs)]) # 11. indexten başla kursun sonuna kadar ne varsa al. Yukarıdakinin farklı bir yazımı. Çıktı= Programlama
+
+print(kurs[5:]) #Başlanğıç indexi verilip sonu verilmezse yine sonuna kadar gider. Çıktı= n ile Programlama
 
 
