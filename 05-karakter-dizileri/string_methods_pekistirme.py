fc7a30373cf345ec74074ee1bd3e84c7c51a6e34
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-24 00:11:43 +0300

    Create string_methods_pekistirme.py

---
+++ 05-karakter-dizileri/string_methods_pekistirme.py
@@ -0,0 +1,22 @@
+kursAdi = "BTK Akademi Python ile programlama dersleri"
+website = "https://www.btkakademi.gov.tr/"
+
+# 1- " BTK Akademi " karakter dizisinin baş ve sondaki boşluk karakterlerini siliniz.
+
+# 2- kursAdi değişkenindeki tüm karakterleri küçük harfe çeviriniz.
+
+# 3- website değişkeninde kaç yane "." karakteri vardır?
+
+# 4- website değişkeni "https" ile mi başlıyor?
+
+# 5- website "tr ile mi bitiyor"?
+
+# 6- kursAdi içerisindeki tüm karakterler harflerden mi oluşuyor?
+
+# 7- kursAdi değişkenindeki tüm boşlukları "-" ile değiştiriniz.
+
+# 8- kursAdi değişkenindeki Python kelimesinin ReactJs ile değiştiriniz.
+
+# 9- website değişkeni "www" içeriyor mu?
+
+# 10- kursAdi değişkenini listeye çeviriniz.
