dc052a49ba695cf42f625a19caab5ccc79e6a15a
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-23 23:29:24 +0300

    Update string_pekiştirme.py

--- 05-karakter-dizileri/string_pekiştirme.py
+++ 05-karakter-dizileri/string_pekiştirme.py
@@ -2,11 +2,29 @@ title="Python ile programlama dersleri"
 
 # 1- "title" değişken içerisindeki karakter sayısı nedir?
 
+# adet= len(title)
+# print(adet)
+
+#-----------------------------------------------------
+
 # 2- "title" içerisindeki "Python" kelimesini alın.
+
+#print(title[:6])
+
+#-----------------------------------------------------
 
 # 3- "title" değişkeninin ilk 5 ve son 5 karakterini alın.
 
+print(title[:6])
+print(title[-8:])
+
+#-----------------------------------------------------
+
 # 4- "title" değişkenini tersten yazdırınız.
+
+
+
+
 
 # 5- Klavyeden girilen öğrenci bilgisine göre örnek verilen cümleyi yazdırınız.
 #   Örnek: Çınar Turan isimli öğrencinin 1.notu 60, 2.notu 60 ve ortalaması 60 olarak hesaplanmıştır.
