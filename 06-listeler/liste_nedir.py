3188ef6f32685f7fdbc7ba23851a7b1935587d4a
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-24 23:16:41 +0300

    Update liste_nedir.py

--- 06-listeler/liste_nedir.py
+++ 06-listeler/liste_nedir.py
@@ -22,7 +22,15 @@ print(type(isimler[0])) #Listedeki ilk elemanın tipi str
 
 ogrenci=["Çınar","Turan",60,50,70]
 
-print(ogrenci[0] + " " + ogrenci[1])
 
+print(ogrenci[0] + " " + ogrenci[1])
 ortalama = (ogrenci[2] + ogrenci[3] + ogrenci[4])/3
 print(ortalama)
+
+
+ogrenciler = [["Çınar","Turan",60,50,70],["Ali","Turan",80,50,70]]
+
+print(ogrenciler[0][0]) #ilk [0]= ogrenciler değişkeninin ilk listesi. ikinci [0]= oluşan listenin ilk elemanı.
+
+print(ogrenciler[1][2]) #[1]= ogrenciler değişkeninin ikinci listesi. [2]= oluşan listenin üçüncü elemanı.
+
