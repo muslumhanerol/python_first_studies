28fa8e31524b3601f7357a3b798f68b7ff0445b0
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-25 22:57:47 +0300

    list_methods

---
+++ 06-listeler/4_liste_methods.py
@@ -0,0 +1,10 @@
+sayilar = [4,6,8,2,56,78,90]
+isimler = ["Ahmet","Canan","Zeynep","Gökhan","Ali"]
+
+sonuc = min(sayilar) # En küçük sayıyı verir.
+sonuc = min(isimler) # Alfabetik olarak ilk harflerden ilklere doğru sıralar.
+
+sonuc = max(sayilar) # En büyük sayıyı verir.
+sonuc = max(isimler) # Alfabetik olarak son harflerden ilklere doğru sıralar.
+
+print(sonuc)
