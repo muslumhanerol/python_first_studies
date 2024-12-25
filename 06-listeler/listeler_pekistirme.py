5394b41ce5d9d3042b9a77f0500d77ff4f585c36
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-25 22:18:43 +0300

    Update listeler_pekistirme.py

---
+++ 06-listeler/listeler_pekistirme.py
@@ -0,0 +1,25 @@
+# 1- "Toyota, Bmw, Renault, Mercedes" elemanlarına sahip markalar isimli bir liste oluşturunuz.
+
+# 2- Liste kaç elemanlıdır?
+
+# 3- Lşstenşn ilk ve son elemanı nedir?
+
+# 4- "Renault" markasını "Togg" ile güncelleyiniz.
+
+# 5- "Togg" listenin bir elemanı mıdır?
+
+# 6- Listenin il 2 elemanını alınır.
+
+# 7- Listenin sonuna "Ford" ve "Citroen" markalarını ekleyiniz.
+
+# 8- Listenin son elemanını siliniz.
+
+# 9- Aşağıdaki verileri liste içerisinde saklayınız.
+
+    # Öğrenci1: Yiğit Bilgi 2010 [70,80,90]
+    # Öğrenci2: Ada Bilgi 2011 [70,70,80] 
+    # Öğrenci3: Çınar Turan 2017 [60,60,90]
+
+# 10- Öğrencilerin yaşlarını hesaplayınız.
+
+# 11- Örencilerin not ortalamalarını hesaplayınız.    
