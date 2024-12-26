d983bf6cf8c977e0e25a7f562dc3c171fc672e60
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-12-26 23:26:05 +0300

    tuple

---
+++ 06-listeler/6_tuple_list.py
@@ -0,0 +1,14 @@
+# İçeriği değiştirilemez, silinemez listeler.
+
+my_list = [1,2,3]
+
+my_tuple = (1,2,4) #değiştirilemez. #Bir başka yazım tekniği 1,2,3 
+
+print(type(my_list))
+print(type(my_tuple))
+
+
+sonuc = my_list[0]
+sonuc = my_tuple[0]
+
+print(sonuc)
