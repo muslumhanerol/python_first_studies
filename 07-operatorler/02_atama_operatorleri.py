1063ad232d16368b80dc9b2067b1edc505a7b960
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-01 23:35:06 +0300

    atama_oper

--- 07-operatorler/02_atama_operatorleri.py
+++ 07-operatorler/02_atama_operatorleri.py
@@ -3,10 +3,29 @@ b = 20
 c = 30
 
 
-a,b,c = 10,20,30 #Tek satırda sırasıyla atama.
+# a,b,c = 10,20,30 #Tek satırda sırasıyla atama.
+# a,b,c = 10,20,(30,40) # 2 değer tuple olarak c değerine karşılık.
+# d = 10,20
 
-a,b,c = 10,20,(30,40) # 2 değer tuple olarak c değerine karşılık.
+a += 5  # a = a + 5 # a nın içerisinde ne varsa onu 5 le topla ve sonucu tekrar a ya at.
+a -= 5  # a = a - 5 # a nın içerisinde ne varsa onudan 5 çıkar ve sonucu tekrar a ya at.
+a *- 5  # a = a * 5 # a nın içerisinde ne varsa onu 5 le çarp ve sonucu tekrar a ya at.
+a = 10
+b = 20
+c = 30
 
-d = 10,20
+
+# a,b,c = 10,20,30 #Tek satırda sırasıyla atama.
+# a,b,c = 10,20,(30,40) # 2 değer tuple olarak c değerine karşılık.
+# d = 10,20
+
+a += 5  # a = a + 5 # a nın içerisinde ne varsa onu 5 le topla ve sonucu tekrar a ya at.
+a -= 5  # a = a - 5 # a nın içerisinde ne varsa onudan 5 çıkar ve sonucu tekrar a ya at.
+a *= 5  # a = a * 5 # a nın içerisinde ne varsa onu 5 le çarp ve sonucu tekrar a ya at.
+a /= 5  # a = a / 5 # a nın içerisinde ne varsa onu 5 e böl ve sonucu tekrar a ya at. Sonuç float olarak döner.
+#a %= 5  # a = a % 5 # mod 5 inin aldığımızda sonucu tekrar a ya at.
+a **= 5  # a = a ** 5 # a nın içerisinde ne varsa onu 5 le onu sayıyla tekrar çarp ve sonucu tekrar a ya at.
+a //= 5  # a = a // 5
+
 
 print(a,b,c)
