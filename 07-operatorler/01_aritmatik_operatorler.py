598ecb51cc9e537040dcef12ae7207e70459bd08
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-01 23:24:27 +0300

    operatorler

---
+++ 07-operatorler/01_aritmatik_operatorler.py
@@ -0,0 +1,15 @@
+a = 9
+b = 2
+
+sonuc = a + b
+sonuc = a - b
+sonuc = a * b
+sonuc = a / b
+sonuc = a % b
+sonuc = a ** b # 5 üzeri 2 yani 9 un yanyana iki kere çarpımı.
+sonuc = a // b # Kalansız bölme operatörü. 9 un içerisinde 2 4 kere var.
+
+
+
+
+print(sonuc)
---
+++ 07-operatorler/02_atama_operatorleri.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+c = 30
+
+
+a,b,c = 10,20,30 #Tek satırda sırasıyla atama.
+
+a,b,c = 10,20,(30,40) # 2 değer tuple olarak c değerine karşılık.
+
+d = 10,20
+
+print(a,b,c)
