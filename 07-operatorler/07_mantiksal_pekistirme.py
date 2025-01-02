b06bd65cde9e961c6656540c40ff9d0a18f38593
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-02 23:30:15 +0300

    Create 07_mantiksal_pekistirme.py

---
+++ 07-operatorler/07_mantiksal_pekistirme.py
@@ -0,0 +1,9 @@
+# 1- Yaşı 18' den büyük ya da  veli izni varsa bir işte çalışabilir durumunu kontrol ediniz.
+
+# 2- Ders notu 50-100 arasındaysa geçti deilse kalsın bilgisini yazdırınız.
+
+# 3- Not ortalaması en az 70 puan ve zayıfı yoksa teşekkür belgesi alabilme durumunu kontrol ediniz.
+
+# 4- İşe girmek için en az önlisans ya da lisans mezunu olma durumunu kontrol ediniz. Sigara kullanmama koşulu.
+
+# 5- Uygulamaya giriş kontrolünü "username yada email" ve "parola" için yapalım.
