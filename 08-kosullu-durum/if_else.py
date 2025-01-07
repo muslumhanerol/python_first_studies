dd1a601010a704d4f7c9cc230765e81f32cfd43f
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-07 23:34:00 +0300

    if/else

---
+++ 08-kosullu-durum/if_else.py
@@ -0,0 +1,13 @@
+#parantez içi doğru değer ürettiği sürece çıktı alınır.
+
+sonuc = 3 > 2
+
+if (sonuc):
+    print("Merhaba") # Ekrana Merhaba yazar.
+
+
+
+sonuc = 3 > 7
+
+if (sonuc):
+    print("Merhaba") # Ekrana Merhaba yazmaz.
