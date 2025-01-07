ecdc7d642f71415585bb805ed3273cbc69915838
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-07 23:38:23 +0300

    Update if_else.py

--- 08-kosullu-durum/if_else.py
+++ 08-kosullu-durum/if_else.py
@@ -1,13 +1,25 @@
 #parantez içi doğru değer ürettiği sürece çıktı alınır.
 
-sonuc = 3 > 2
+# sonuc = 3 > 2
 
-if (sonuc):
-    print("Merhaba") # Ekrana Merhaba yazar.
+# if (sonuc):
+#     print("Merhaba") # Ekrana Merhaba yazar.
 
 
 
-sonuc = 3 > 7
+# sonuc = 3 > 7
 
-if (sonuc):
-    print("Merhaba") # Ekrana Merhaba yazmaz.
+# if (sonuc):
+#     print("Merhaba") # Ekrana Merhaba yazmaz.
+
+#-----------------------------------------------------------------
+
+email = "akademi@btk.com"
+parola = "123456"
+
+login = (email == "akademi@btk.com") and (parola == "123456")
+
+if(login):
+    print("Login olundu.")
+else:
+    print("email yada parola yanlış.")
