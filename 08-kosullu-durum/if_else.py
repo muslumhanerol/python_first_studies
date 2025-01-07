47b1401ccc8538d043f59120017e38a071a963ab
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-07 23:51:09 +0300

    if/else_login

--- 08-kosullu-durum/if_else.py
+++ 08-kosullu-durum/if_else.py
@@ -14,12 +14,44 @@
 
 #-----------------------------------------------------------------
 
+# email = "akademi@btk.com"
+# parola = "123456"
+
+# login = (email == "akademi@btk.com") and (parola == "123456")
+
+# if(login):
+#     print("Login olundu.")
+# else:
+#     print("email yada parola yanlış.")
+
+#-----------------------------------------------------------------
+# not operatörlü kullanım.
+
+# email = "akademi@btk.com"
+# parola = "12345"
+
+# login = (email == "akademi@btk.com") and (parola == "12345")
+
+# if(not(login)): # not = tam tersi soru sorar. login false cevabını getirirse tam tersi cevap verir.
+#     print("email yada parola yanlış.")
+# else:
+#     print("Login olundu.")   
+    
+    
+
+#-----------------------------------------------------------------
+
+# Hangi parametrenin doğru ve yanlış olduğu ayrımını yapma.
+
 email = "akademi@btk.com"
 parola = "123456"
 
-login = (email == "akademi@btk.com") and (parola == "123456")
 
-if(login):
-    print("Login olundu.")
+
+if(email == "akademi@btk.com"):
+    if(parola == "123456"):
+        print("Login olundu.")
+    else:
+        print("Parola yanlış")    
 else:
-    print("email yada parola yanlış.")
+    print("email yanlış.") 
