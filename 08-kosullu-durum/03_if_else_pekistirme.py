b89cb25cb400b91cfb40d5bbb9e39393538ab2c3
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-08 23:38:28 +0300

    Create 03_if_else_pekistirme.py

---
+++ 08-kosullu-durum/03_if_else_pekistirme.py
@@ -0,0 +1,16 @@
+# Bir aracın yakıt tipine göre (benzin, dizel, lpg) belirtilen bir mesafede ne kadar yakıt masrafı olduğunu hesaplayan uygulamayı yapınız.
+
+# benzin  : 39.35
+# dizel   : 41,71
+# lpg     : 20.28
+
+
+
+# Bir öğrencinin 2 yazılı bir sözlü notunu alarak ortalama hesaplayınız ve hesaplanan ortalamaya göre not aralığına karşılık gelen değerlendirmeyi yazdırınız.
+
+# 0  - 24   => 0
+# 25 - 44   => 1
+# 45 - 54   => 2
+# 55 - 69   => 3
+# 70 - 84   => 4
+# 85 - 100  => 5
