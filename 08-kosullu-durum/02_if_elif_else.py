f3162c69b787227dcd6a257e6370c5c659c63c2c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-08 23:27:30 +0300

    Update 02_if_elif_else.py

---
+++ 08-kosullu-durum/02_if_elif_else.py
@@ -0,0 +1,15 @@
+email = "akademi@btk.com"
+parola = "123456"
+
+
+
+if(email != "akademi@btk.com"):
+      print("email yanlış.")
+    
+elif(parola != "123456"):
+        print("Parola yanlış.")
+        
+else:
+    print("email yanlış.")
+
+
