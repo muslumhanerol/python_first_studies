94a52e5da818e6eb6944f5738d9a8eab7b97ede6
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-01-08 23:32:53 +0300

    Update 02_if_elif_else.py

--- 08-kosullu-durum/02_if_elif_else.py
+++ 08-kosullu-durum/02_if_elif_else.py
@@ -1,15 +1,27 @@
-email = "akademi@btk.com"
-parola = "123456"
+# email = "akademi@btk.com"
+# parola = "123456"
 
 
 
-if(email != "akademi@btk.com"):
-      print("email yanlış.")
+# if(email != "akademi@btk.com"):
+#       print("email yanlış.")
     
-elif(parola != "123456"):
-        print("Parola yanlış.")
+# elif(parola != "123456"):
+#         print("Parola yanlış.")
         
+# else:
+#     print("login olundu")
+
+#---------------------------------------------------------------------------------------
+
+x = 10
+y = 20
+
+if(x < y):
+    print("x y den küçük")
+elif(y < x):
+    print("y x den küçük")
 else:
-    print("email yanlış.")
+    print("x y eşit") 
 
 
